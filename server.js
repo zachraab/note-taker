2213ad726c8c0bfbd6dc094b9d7ce3f7eb195add
Zach Raab <zpraab@hotmail.com>
2021-05-27 22:43:13 -0600

    port bug fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 // TODO change port
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing JSON
 
